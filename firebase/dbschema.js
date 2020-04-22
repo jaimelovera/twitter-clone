b5fa7cd3bdce06fb933efc5e318b0307ecc6d549
Jaime Lovera <lovera.jaime@gmail.com>
2020-04-22 08:17:49 -0700

    setup express and made my first cloud function, a post request

---
+++ firebase/dbschema.js
@@ -0,0 +1,13 @@
+// Just a reference file to see my database schema.
+
+let db = {
+  posts: [
+    {
+      userHandle: "user",
+      body: "this is a post body",
+      createdAt: "2020-04-22T14:48:28.015Z",
+      likeCount: 1,
+      commentCount: 1,
+    },
+  ],
+};
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,32 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
+admin.initializeApp();
+
+const express = require("express");
+const app = express();
+
+app.post("/post", (req, res) => {
+  const newPost = {
+    userHandle: req.body.userHandle,
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created succesfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.log(err);
+    });
 });
+
+// This app function will be a container for all of our functions.
+// It will add "/api" to all of our api routes.
+// GET/POST will be explicitly stated for each function so no need to check.
+exports.api = functions.https.onRequest(app);
