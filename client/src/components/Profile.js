700ade80ff500fc05e1fdc304623f7340293062e
Jaime Lovera <lovera.jaime@gmail.com>
2020-05-09 21:13:26 -0700

    installed material-ui icons

---
+++ client/src/components/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// Material-UI Stuff
+import Button from "@material-ui/core/Button";
+
+// Icons
+
+class Profile extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Profile;
